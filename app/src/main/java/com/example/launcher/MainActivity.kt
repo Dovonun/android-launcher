2b38827e5c1cd7055bd70aa6330939921f13912d
dovonun <31408384+Dovonun@users.noreply.github.com>
2025-01-27 23:30:08 +0100

    fix wallpaper

--- app/src/main/java/com/example/launcher/MainActivity.kt
+++ app/src/main/java/com/example/launcher/MainActivity.kt
@@ -5,19 +5,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.WindowInsets
+import android.view.WindowManager
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -25,13 +25,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -49,9 +48,7 @@ import androidx.compose.ui.unit.times
 import androidx.core.graphics.drawable.toBitmap
 
 data class App(
-    val name: String,
-    val packageName: String,
-    val icon: ImageBitmap?
+    val name: String, val packageName: String, val icon: ImageBitmap
 )
 
 fun App.launch(context: Context) {
@@ -61,8 +58,6 @@ fun App.launch(context: Context) {
 
 fun getInstalledApps(context: Context): List<App> {
     val packageManager = context.packageManager
-
-    // Query all apps with a launcher intent (apps that can be launched)
     val intent = Intent(Intent.ACTION_MAIN, null).apply {
         addCategory(Intent.CATEGORY_LAUNCHER)
     }
@@ -76,208 +71,173 @@ fun getInstalledApps(context: Context): List<App> {
     }
 }
 
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("MainActivity", "onCreate called")
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER,
+            WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER
+        )
+        window.attributes.setWallpaperTouchEventsEnabled(false)
+
         enableEdgeToEdge()
         setContent {
             window.insetsController?.hide(WindowInsets.Type.statusBars())
-
             val context = LocalContext.current
-            val installedApps = getInstalledApps(context).sortedBy { it.name }
+
+            val installedApps by remember { mutableStateOf(getInstalledApps(context).sortedBy { it.name }) }
             val groupedApps =
                 remember { mutableStateOf(installedApps.groupBy { it.name[0].uppercaseChar() }) }
             val sortedLetters = remember { groupedApps.value.keys.sorted() }
-
             var selectedLetter by remember { mutableStateOf<Char?>(null) }
-            var isScrollbarTouched by remember { mutableStateOf(false) }
-            val lazyListState = rememberLazyListState()
 
-            LaunchedEffect(selectedLetter) {
-                selectedLetter?.let { letter ->
-                    val index = installedApps.indexOfFirst { it.name[0].uppercaseChar() == letter }
-                    if (index != -1) {
-                        lazyListState.scrollToItem(index)
-//                        vibrate()
-                    }
-                }
+            val sharedPreferences by remember {
+                mutableStateOf(
+                    context.getSharedPreferences(
+                        "launcher_prefs", Context.MODE_PRIVATE
+                    )
+                )
             }
+            var favorites by remember {
+                mutableStateOf(
+                    sharedPreferences.getStringSet("favorites", emptySet()) ?: emptySet()
+                )
+            }
+            var selectedApp by remember { mutableStateOf<App?>(null) }
+            if (selectedApp != null) {
+                AlertDialog(onDismissRequest = { selectedApp = null },
+                    title = { Text(text = selectedApp!!.name) },
+                    text = {
+                        Text(
+                            if (favorites.contains(selectedApp!!.packageName)) {
+                                "Remove ${selectedApp!!.name} from favorites?"
+                            } else {
+                                "Add ${selectedApp!!.name} to favorites?"
+                            }
+                        )
+                    },
+                    confirmButton = {
+                        Button(onClick = {
+                            val newFavorites = if (favorites.contains(selectedApp!!.packageName)) {
+                                favorites.filter { it != selectedApp!!.packageName }.toSet()
+                            } else {
+                                favorites + selectedApp!!.packageName
+                            }
+                            sharedPreferences.edit().putStringSet("favorites", newFavorites).apply()
+                            favorites = newFavorites
+                            selectedApp = null
+                        }) { Text("Yes") }
+                    })
+            }
+
             val filteredApps = selectedLetter?.let { letter ->
                 installedApps.filter { it.name[0].uppercaseChar() == letter }
             } ?: installedApps
 
+            //debug texts
+            Column {
+                Text(text = selectedLetter?.toString() ?: "Foo")
+                Text(text = selectedApp?.toString() ?: "Selected App")
+                Text(text = favorites.toString() ?: "Selected App")
+            }
 
             Box(modifier = Modifier.fillMaxSize()) {
-                // Display the list of apps
-                LazyColumn(
-                    state = lazyListState, modifier = Modifier
-                        .fillMaxSize()
-                        .padding(top = 1f / 4f * LocalConfiguration.current.screenHeightDp.dp)
-                ) {
-                    // Add the selected letter as a header
-                    selectedLetter?.let { letter ->
-                        item {
+                if (selectedLetter == null) {
+                    LazyColumn(
+                        modifier = Modifier.padding(top = 1f / 4f * LocalConfiguration.current.screenHeightDp.dp)
+                    ) {
+                        items(installedApps.filter { app -> favorites.contains(app.packageName) },
+                            key = { it.packageName }) { app ->
+                            AppRow(app = app,
+                                launchApp = { app.launch(context) },
+                                toggleFavorites = { selectedApp = app })
+                        }
+                    }
+                } else {
+                    LazyColumn(
+                        modifier = Modifier.padding(top = 1f / 4f * LocalConfiguration.current.screenHeightDp.dp)
+                    ) {
+                        item(key = "header") {
                             Text(
-                                text = letter.toString(),
+                                text = selectedLetter.toString(),
                                 fontSize = 24.sp,
                                 color = Color.Black,
                                 modifier = Modifier
-                                    .fillMaxWidth()
                                     .padding(16.dp)
                                     .padding(start = 64.dp)
                             )
                         }
-                    }
-                    items(filteredApps) { app ->
-                        AppRow(app = app) {
-                            app.launch(context)
+                        items(filteredApps, key = { it.packageName }) { app ->
+                            AppRow(app = app,
+                                launchApp = { app.launch(context) },
+                                toggleFavorites = { selectedApp = app })
                         }
                     }
                 }
-
-
-                Box(
+                LetterBar(
+                    sortedLetters = sortedLetters,
+                    setSelectedLetter = { selectedLetter = it },
                     modifier = Modifier
                         .align(Alignment.CenterEnd)
-                        .fillMaxHeight()
                         .padding(top = 1f / 3f * LocalConfiguration.current.screenHeightDp.dp) // Start 1/3 from the top
                         .padding(bottom = 1f / 8f * LocalConfiguration.current.screenHeightDp.dp) // End 1/8 from the bottom
-                        .border(1.dp, Color.Green)
-                ) {
-                    Row(
-                        modifier = Modifier
-                            .align(Alignment.CenterEnd)
-                            .fillMaxHeight()
-                            .border(1.dp, Color.Blue)
-                    ) {
-                        // Selected letter
-                        if (isScrollbarTouched) {
-                            Box(
-                                modifier = Modifier
-                                    .fillMaxHeight()
-                                    .width(48.dp)
-                                    .background(Color.Transparent)
-                            ) {
-                                // Display the alphabet vertically
-                                Column(
-                                    modifier = Modifier
-                                        .fillMaxHeight()
-                                        .padding(8.dp),
-                                    verticalArrangement = Arrangement.SpaceBetween // Evenly distribute letters
-                                ) {
-                                    sortedLetters.forEach { letter ->
-                                        Text(
-                                            text = letter.toString(),
-                                            fontSize = if (letter == selectedLetter) 24.sp else 16.sp, // Make the selected letter bigger
-                                            color = if (letter == selectedLetter) Color.Red else Color.Gray,
-                                            modifier = Modifier.padding(2.dp)
-                                        )
-                                    }
-                                }
-                            }
-                        }
-                        Box(
-                            modifier = Modifier
-                                .fillMaxHeight()
-                                .width(48.dp)
-                                .background(Color.Transparent)
-                                .border(1.dp, Color.Gray)
-                                .pointerInput(Unit) {
-                                    detectDragGestures(
-                                        onDragStart = {
-                                            isScrollbarTouched = true
-                                        },
-                                        onDragEnd = {
-                                            isScrollbarTouched = false
-                                        },
-                                        onDrag = { change, _ ->
-                                            val letterIndex =
-                                                (change.position.y / size.height * sortedLetters.size).toInt()
-                                            selectedLetter = sortedLetters.getOrNull(letterIndex)
-                                        }
-                                    )
-                                }
-                        )
-                    }
-                }
-//                AlphabetScrollbar(
-//                    letters = sortedLetters,
-//                    onLetterSelected = { letter ->
-//                        selectedLetter = letter
-//                    }
-//                )
+                )
             }
         }
     }
 }
 
 @Composable
-fun AppRow(app: App, modifier: Modifier = Modifier, onClick: () -> Unit) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically, modifier = modifier
-            .fillMaxWidth()
-            .padding(start = 64.dp)
-            .padding(8.dp)
-            .clickable(onClick = onClick)
-    ) {
-        app.icon?.let { icon ->
-            Image(
-                bitmap = icon,
-                contentDescription = app.name,
-                modifier = modifier.size(42.dp)
-            )
+fun LetterBar(
+    modifier: Modifier = Modifier, sortedLetters: List<Char>, setSelectedLetter: (Char?) -> Unit
+) {
+    var selectedLetter by remember { mutableStateOf<Char?>(null) }
+    Row(modifier = modifier) {
+        var isScrollbarTouched by remember { mutableStateOf(false) }
+        Column(verticalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxHeight()
+                .width(if (isScrollbarTouched) 96.dp else 48.dp)
+                .border(1.dp, Color.Gray)
+                .pointerInput(Unit) {
+                    detectDragGestures(onDragStart = { isScrollbarTouched = true },
+                        onDragEnd = { isScrollbarTouched = false },
+                        onDrag = { change, _ ->
+                            val letterIndex =
+                                (change.position.y / size.height * sortedLetters.size).toInt()
+                            selectedLetter = sortedLetters[letterIndex]
+                            setSelectedLetter(selectedLetter)
+                        })
+                }) {
+            sortedLetters.forEach { letter ->
+                if (selectedLetter != null) {
+                    Text(
+                        text = letter.toString(),
+                        fontSize = if (letter == selectedLetter) 24.sp else 16.sp, // Make the selected letter bigger
+                        color = if (letter == selectedLetter) Color.Red else Color.Gray,
+                        modifier = Modifier
+                    )
+                }
+            }
         }
-        Spacer(modifier = modifier.width(32.dp))
-        Text(
-            text = app.name, style = MaterialTheme.typography.bodyLarge,
-            modifier = modifier
-        )
     }
 }
-//
-//@Composable
-//fun AlphabetScrollbar(
-//    letters: List<Char>,
-//    onLetterSelected: (Char) -> Unit
-//) {
-//    Box(
-//        modifier = Modifier
-//            .align(Alignment.CenterEnd) // Align to the right
-//            .fillMaxHeight()
-//            .width(48.dp)
-//            .background(Color.Transparent)
-//            .pointerInput(Unit) {
-//                var onDragStart = {
-//                    isScrollbarTouched = true
-//                },
-//                var onDragEnd = {
-//                    isScrollbarTouched = false
-//                },
-//                var onDrag = { change, _ ->
-//                    val letterIndex =
-//                        (change.position.y / size.height * letters.size).toInt()
-//                    val selectedLetter = letters.getOrNull(letterIndex)
-//                    selectedLetter?.let { onLetterSelected(it) }
-//                }
-//            }
-//    ) {
-//        if (isScrollbarTouched) {
-//            // Display the alphabet vertically
-//            Column(
-//                modifier = Modifier
-//                    .align(Alignment.CenterEnd)
-//                    .padding(8.dp)
-//            ) {
-//                letters.forEach { letter ->
-//                    Text(
-//                        text = letter.toString(),
-//                        color = if (letter == selectedLetter) Color.White else Color.Gray,
-//                        modifier = Modifier.padding(2.dp)
-//                    )
-//                }
-//            }
-//        }
-//    }
-//}
+
+@Composable
+fun AppRow(
+    app: App, modifier: Modifier = Modifier, launchApp: () -> Unit, toggleFavorites: () -> Unit
+) {
+    Row(verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .padding(start = 64.dp)
+            .padding(8.dp)
+            .pointerInput(Unit) {
+                detectTapGestures(onTap = { launchApp() }, onLongPress = { toggleFavorites() })
+            }) {
+        Image(bitmap = app.icon, contentDescription = app.name, modifier = modifier.size(42.dp))
+        Spacer(modifier = modifier.width(32.dp))
+        Text(text = app.packageName, style = MaterialTheme.typography.bodyLarge)
+    }
+}
