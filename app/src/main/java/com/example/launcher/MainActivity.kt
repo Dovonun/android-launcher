c4c7a8f9b19fdfbbff01cd94965081d6940ac0dd
Dovonun <31408384+Dovonun@users.noreply.github.com>
2025-11-08 10:06:43 +0100

    theme works but loos not that great

--- app/src/main/java/com/example/launcher/MainActivity.kt
+++ app/src/main/java/com/example/launcher/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.launcher
 
-import android.R.color.transparent
+import android.R
 import android.annotation.SuppressLint
 import android.app.WallpaperColors
 import android.app.WallpaperManager
@@ -52,6 +52,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.produceState
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -75,8 +76,10 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.times
 import androidx.compose.ui.window.Popup
 import androidx.compose.ui.window.PopupProperties
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
-import android.view.WindowInsets as ViewWindowInsets
+import kotlinx.coroutines.launch
 
 const val H_PAD = 24
 const val H_PAD2 = 2 * H_PAD
@@ -88,6 +91,9 @@ private fun buildBarIndex(apps: Map<Char, List<UiRow>>): List<Int> = buildList {
     }
 }
 
+// TODO: Next AI chat about themes | Material3 in compose
+// TODO: Next Make it look nice. Can you do the outline thing on text?
+// Current version is more bold. Seems better
 class MainActivity : ComponentActivity() {
     private val viewVM: ViewVM by viewModels()
 
@@ -96,106 +102,92 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-        window.attributes.setWallpaperTouchEventsEnabled(false)
-        window.setBackgroundDrawableResource(transparent)
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+        controller.hide(WindowInsetsCompat.Type.systemBars())
         setContent {
-            window.insetsController?.hide(ViewWindowInsets.Type.statusBars())
-            val context = LocalContext.current
-            val haptic = LocalHapticFeedback.current
-            val appsVM: AppsVM = viewModel()
-            val systemVM: SystemVM = viewModel()
-            val wallpaperManager = WallpaperManager.getInstance(context)
-            val wallpaperColors: WallpaperColors? =
-                remember { wallpaperManager.getWallpaperColors(WallpaperManager.FLAG_SYSTEM) }
+            LauncherTheme {
+                val appsVM: AppsVM = viewModel()
+                val systemVM: SystemVM = viewModel()
+                val listState = rememberLazyListState()
 
-            val primaryColorInt: Int? = remember { wallpaperColors?.primaryColor?.toArgb() }
-            val primaryColor = remember { primaryColorInt?.let { Color(it) } ?: Color.Black }
-            val listState = rememberLazyListState()
-//            val primaryColorHsv = remember { FloatArray(3) }
-//            val bright_primaryColor = remember { Color.hsv(primaryColor.hue, 1f, 1f) }
+                val menu by viewVM.menu.collectAsState()
+                when (val state = menu) {
+                    is MenuState.Popup -> ShortcutPopup(state, appsVM, viewVM)
+                    is MenuState.Sheet -> ContextSheet(
+                        state, appsVM
+                    ) { viewVM.setMenu(MenuState.None) }
 
-            val view by viewVM.view.collectAsState()
-            LaunchedEffect(view) {
-                val v = view
-                if (v is View.AllApps) {
-                    haptic.performHapticFeedback(HapticFeedbackType.TextHandleMove)
-                    listState.scrollToItem(v.index, 0)
-                }
-            }
-            val menu by viewVM.menu.collectAsState()
-            LaunchedEffect(menu) {
-                when (menu) {
-                    is MenuState.Popup -> haptic.performHapticFeedback(HapticFeedbackType.LongPress)
-                    is MenuState.Sheet -> haptic.performHapticFeedback(HapticFeedbackType.LongPress)
                     is MenuState.None -> Unit
                 }
-            }
-            when (val state = menu) {
-                is MenuState.Popup -> ShortcutPopup(state, appsVM, viewVM)
-                is MenuState.Sheet -> ContextSheet(state, appsVM) { viewVM.setMenu(MenuState.None) }
-                is MenuState.None -> Unit
-            }
-            val allApps by appsVM.uiAllGrouped.collectAsState()
-            val scrollIndexes by remember(allApps) { derivedStateOf { buildBarIndex(allApps) } }
-            val favorites by appsVM.favorites.collectAsState()
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.hsv(0f, 0.0f, 0f, 0.15f))
-            ) {
-                when (view) {
-                    is View.Favorites -> Column(
-                        verticalArrangement = Arrangement.Bottom,
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .padding(start = H_PAD2.dp)
-                            .padding(bottom = 1f / 8f * LocalConfiguration.current.screenHeightDp.dp)
-                            .pointerInput(Unit) {
-                                detectVerticalDragGestures(
-                                    onVerticalDrag = { change, dragAmount ->
-                                        if (change.isConsumed) return@detectVerticalDragGestures
-                                        if (dragAmount > 60f) systemVM.expandNotificationShade()
-                                    })
-                            }) {
-                        favorites.forEach { fav -> IconRow(fav, appsVM, viewVM) }
-                    }
+                val allApps by appsVM.uiAllGrouped.collectAsState()
+                val scrollIndexes by remember(allApps) { derivedStateOf { buildBarIndex(allApps) } }
+                val favorites by appsVM.favorites.collectAsState()
+                val view by viewVM.view.collectAsState()
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.hsv(0f, 0.0f, 0f, 0.15f))
+                ) {
+                    when (view) {
+                        is View.Favorites -> Column(
+                            verticalArrangement = Arrangement.Bottom,
+                            modifier = Modifier
+                                .fillMaxHeight()
+                                .padding(start = H_PAD2.dp)
+                                .padding(bottom = 1f / 8f * LocalConfiguration.current.screenHeightDp.dp)
+                                .pointerInput(Unit) {
+                                    detectVerticalDragGestures(
+                                        onVerticalDrag = { change, dragAmount ->
+                                            if (change.isConsumed) return@detectVerticalDragGestures
+                                            if (dragAmount > 60f) systemVM.expandNotificationShade()
+                                        })
+                                }) {
+                            favorites.forEach { fav -> IconRow(fav, appsVM, viewVM) }
+                        }
 
-                    is View.AllApps -> LazyColumn(
-                        modifier = Modifier.padding(start = H_PAD2.dp),
-                        state = listState,
-                        contentPadding = PaddingValues(
-                            top = 1f / 3f * LocalConfiguration.current.screenHeightDp.dp,
-                            bottom = 2f / 3f * LocalConfiguration.current.screenHeightDp.dp
-                        )
-                    ) {
-                        appsVM.uiAllGrouped.value.forEach { (letter, list) ->
-                            item {
-                                Text(
-                                    text = letter.toString(),
-                                    fontSize = 24.sp,
-                                    color = primaryColor,
-                                    modifier = Modifier
-                                        .padding(16.dp)
-                                        .padding(start = 64.dp)
-                                )
-                            }
-                            // TODO: What happens when 2 apps have the same name?
-                            items(items = list, key = { "all-${it.label}" }) { row ->
-                                IconRow(row, appsVM, viewVM)
+                        is View.AllApps -> LazyColumn(
+                            modifier = Modifier.padding(start = H_PAD2.dp),
+                            state = listState,
+                            contentPadding = PaddingValues(
+                                top = 1f / 3f * LocalConfiguration.current.screenHeightDp.dp,
+                                bottom = 2f / 3f * LocalConfiguration.current.screenHeightDp.dp
+                            )
+                        ) {
+                            appsVM.uiAllGrouped.value.forEach { (letter, list) ->
+                                item {
+                                    Text(
+                                        text = letter.toString(),
+                                        style = MaterialTheme.typography.headlineLarge.copy(
+                                            shadow = Shadow(
+                                                color = MaterialTheme.colorScheme.primary, offset = Offset(0.01f, 0.01f), blurRadius = 5f
+                                            )
+                                        ),
+                                        color = MaterialTheme.colorScheme.onPrimary,
+                                        modifier = Modifier
+                                            .padding(16.dp)
+                                            .padding(start = 16.dp)
+                                    )
+                                }
+                                // TODO: What happens when 2 apps have the same name?
+                                items(items = list, key = { "all-${it.label}" }) { row ->
+                                    IconRow(row, appsVM, viewVM)
+                                }
                             }
                         }
                     }
+                    val coroutineScope = rememberCoroutineScope()
+                    LetterBar(
+                        view = view, letters = allApps.keys.toList(), update = { index ->
+                            val scrollI = scrollIndexes.elementAtOrNull(index)
+                            if (scrollI == null) viewVM.setView(View.Favorites) else {
+                                viewVM.setView(View.AllApps(scrollI))
+                                coroutineScope.launch {
+                                    listState.scrollToItem(scrollI)
+                                }
+                            }
+                        }, modifier = Modifier.align(Alignment.CenterEnd)
+                    )
                 }
-                LetterBar(
-                    color = primaryColor,
-                    view = view,
-                    letters = allApps.keys.toList(),
-                    update = { index ->
-                        val scrollI = scrollIndexes.elementAtOrNull(index)
-                        viewVM.setView(if (scrollI == null) View.Favorites else View.AllApps(scrollI))
-                    },
-                    modifier = Modifier.align(Alignment.CenterEnd)
-                )
             }
         }
     }
@@ -208,9 +200,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LetterBar(
-    color: Color, // TODO: can this handled via a theme?
     letters: List<Char>, view: View, update: (Int) -> Unit, modifier: Modifier = Modifier
 ) {
+    val haptic = LocalHapticFeedback.current
     var isScrollbarTouched by remember { mutableStateOf(false) }
     var selectedLetter by remember { mutableStateOf<Char?>(null) }
     Column(
@@ -227,17 +219,21 @@ fun LetterBar(
                     onVerticalDrag = { change, _ ->
                         change.consume()
                         val index = (change.position.y / size.height * letters.size).toInt()
-                        selectedLetter = letters.getOrNull(index)
-                        update(index)
+                        val letter = letters.getOrNull(index)
+                        if (letter != selectedLetter) {
+                            haptic.performHapticFeedback(HapticFeedbackType.TextHandleMove)
+                            selectedLetter = letter
+                            update(index)
+                        }
                     })
             }) {
         if (view is View.AllApps) {
             letters.forEach { letter ->
+                val isSelected = selectedLetter == letter
                 Text(
                     text = letter.toString(),
-                    fontSize = if (letter == selectedLetter) 24.sp else 16.sp, // Make the selected letter bigger
-                    color = if (letter == selectedLetter) Color.White else color,
-                    modifier = Modifier
+                    style = if (isSelected) MaterialTheme.typography.headlineLarge else MaterialTheme.typography.headlineSmall,
+                    color = if (isSelected) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.onSurface,
                 )
             }
         }
@@ -245,12 +241,7 @@ fun LetterBar(
 }
 
 @Composable
-fun IconRow(
-    uiRow: UiRow,
-    appVM: AppsVM,
-    viewVM: ViewVM,
-    modifier: Modifier = Modifier,
-) {
+fun IconRow(uiRow: UiRow, appVM: AppsVM, viewVM: ViewVM, modifier: Modifier = Modifier) {
     var layoutCoordinates: LayoutCoordinates? = null
     Row(
         verticalAlignment = Alignment.CenterVertically,
@@ -288,11 +279,10 @@ fun RowIcon(icon: ImageBitmap) =
 @Composable
 fun RowLabel(text: String) = Text(
     text = text,
-    fontSize = 24.sp,
-    color = Color.White,
-    style = MaterialTheme.typography.labelMedium.copy(
+    color = MaterialTheme.colorScheme.onSurface,
+    style = MaterialTheme.typography.headlineMedium.copy(
         shadow = Shadow(
-            color = Color.Black, offset = Offset(0.01f, 0.01f), blurRadius = 5f
+            color = MaterialTheme.colorScheme.surface, offset = Offset(0.01f, 0.01f), blurRadius = 5f
         )
     ),
     maxLines = 1,
@@ -313,12 +303,13 @@ fun SheetEntry(text: String, onClick: () -> Unit, onDismiss: () -> Unit) {
             .height(42.dp) // same as AppRow height
     ) {
         Spacer(modifier = Modifier.width(74.dp)) // for icon space (42 + 32 spacing)
-        Text(text = text, fontSize = 24.sp, color = Color.White)
+        Text(text = text, style = MaterialTheme.typography.bodyLarge, color = MaterialTheme.colorScheme.onSurface)
     }
 }
 
 @Composable
 fun ShortcutPopup(state: MenuState.Popup, appsVM: AppsVM, viewVM: ViewVM) {
+    val haptic = LocalHapticFeedback.current
     val entries by produceState(initialValue = emptyList(), state.item) {
         value = appsVM.popupEntries(state.item)
     }
@@ -327,6 +318,7 @@ fun ShortcutPopup(state: MenuState.Popup, appsVM: AppsVM, viewVM: ViewVM) {
     val screenWidth = LocalConfiguration.current.screenWidthDp.dp
     val maxWidth = screenWidth - H_PAD2.dp
     val reset = { viewVM.setMenu(MenuState.None) }
+    LaunchedEffect(Unit) { haptic.performHapticFeedback(HapticFeedbackType.LongPress) }
     Popup(properties = PopupProperties(focusable = true), onDismissRequest = reset) {
         Box(
             Modifier
@@ -339,7 +331,7 @@ fun ShortcutPopup(state: MenuState.Popup, appsVM: AppsVM, viewVM: ViewVM) {
                     .offset(
                         x = H_PAD.dp, y = (yDp - height - safeTopDp).coerceAtLeast(0.dp)
                     )
-                    .background(Color(0xFF121212), RoundedCornerShape(12.dp))
+                    .background(MaterialTheme.colorScheme.surface, RoundedCornerShape(12.dp))
                     .widthIn(max = maxWidth)
                     .padding(horizontal = H_PAD.dp, vertical = 12.dp),
                 verticalArrangement = Arrangement.Bottom
@@ -363,15 +355,16 @@ fun ShortcutPopup(state: MenuState.Popup, appsVM: AppsVM, viewVM: ViewVM) {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ContextSheet(state: MenuState.Sheet, appsVM: AppsVM, reset: () -> Unit) {
+    val haptic = LocalHapticFeedback.current
     val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
     val entries by produceState(initialValue = emptyList(), state.item) {
         value = appsVM.sheetEntries(state.item)
     }
+    LaunchedEffect(Unit) { haptic.performHapticFeedback(HapticFeedbackType.LongPress) }
     ModalBottomSheet(
         onDismissRequest = reset,
         sheetState = sheetState,
-        containerColor = Color(0xFF121212),
-        shape = RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp),
+        containerColor = MaterialTheme.colorScheme.surface,
         windowInsets = WindowInsets(0.dp)
     ) {
         Column(Modifier.fillMaxWidth()) {
